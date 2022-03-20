e5ae03af6082a012321a384010dba61ce0c624bc
Apostle <malinovski.nik@gmail.com>
2022-03-20 15:53:28 +0300

    some fixes

--- Assets/Scripts/CollisionsChecker.cs
+++ Assets/Scripts/CollisionsChecker.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionsChecker : MonoBehaviour
 {
     [SerializeField] private string rotateTag;
+    [SerializeField] private string finishTag;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +14,12 @@ public class CollisionsChecker : MonoBehaviour
         {
             CommandInvoker.AddCommand(new TakeDamageCommand());
             CommandInvoker.AddCommand(new RotateWorldCommand());
+        }
+
+        if (collision.CompareTag(finishTag))
+        {
+            int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene((nextLevel > SceneManager.sceneCount) ? 0 : nextLevel);
         }
     }
 }
